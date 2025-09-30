f7a23aaf05c74e51325642b0c7a59be94a54e3d2
Gwenaëlle Gustin <gwenaelle.gustin@hevs.ch>
2025-09-30 11:00:14 +0200

    feat: 1 circle responsive

--- 00_boilerplates_VanillaJS/vanilla-empty/sketch.js
+++ 00_boilerplates_VanillaJS/vanilla-empty/sketch.js
@@ -1,1 +1,41 @@
-console.log("hello world");
+function myApp() {
+  const canvas = document.createElement("canvas");
+  document.body.appendChild(canvas);
+
+  // Hardware of mac
+  // const pixelDensity = window.devicePixelRatio;
+  // const width = window.innerWidth * pixelDensity;
+  // const height = window.innerHeight * pixelDensity;
+  // canvas.width = width;
+  // canvas.height = height;
+  // canvas.style.width = width / pixelDensity + "px";
+  // canvas.style.height = height / pixelDensity + "px";
+
+  window.addEventListener("resize", () => {
+    drawCanvas(canvas);
+  });
+  drawCanvas(canvas);
+}
+
+window.addEventListener("load", () => {
+  myApp();
+});
+
+function drawCanvas(canvas) {
+  console.log("draw canvas");
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  const ctx = canvas.getContext("2d");
+  ctx.clearRect(0, 0, width, height);
+
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, width, height);
+
+  ctx.fillStyle = "black";
+
+  ctx.arc(width / 2, height / 2, 100, 0, 2 * Math.PI);
+  ctx.fill();
+}
