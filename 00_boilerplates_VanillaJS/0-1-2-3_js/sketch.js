9c17111cf47289553fb56d51224e56c43a5ea124
Gwenaëlle Gustin <gwenaelle.gustin@hevs.ch>
2025-10-05 16:27:58 +0200

    session 1 Lucien

--- 00_boilerplates_VanillaJS/0-1-2-3_js/sketch.js
+++ 00_boilerplates_VanillaJS/0-1-2-3_js/sketch.js
@@ -14,6 +14,7 @@ function myApp() {
   window.addEventListener("resize", () => {
     drawCanvas(canvas);
   });
+
   drawCanvas(canvas);
 }
 
@@ -30,71 +31,82 @@ function drawCanvas(canvas) {
   const ctx = canvas.getContext("2d");
   ctx.clearRect(0, 0, width, height);
 
+  // Background
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, width, height);
+
+  // 1 circle
   // ctx.fillStyle = "white";
-  // ctx.fillRect(0, 0, width, height);
-  // drawCircles(ctx, width, height);
+  // ctx.arc(100, 100, 50, 0, 2 * Math.PI);
+  // ctx.fill();
+  drawCircles(ctx, width, height);
 
-  let frameCount = 0;
-  drawAnimateCircles();
+  // let frameCount = 0;
+  // drawAnimateCircles();
 
-  function drawAnimateCircles() {
-    ctx.fillStyle = "rgba(255,255,255,0.05)";
-    ctx.fillRect(0, 0, width, height);
-    ctx.fillStyle = "black";
+  // function drawAnimateCircles() {
+  //   ctx.fillStyle = "rgba(255,255,255,0.05)";
+  //   ctx.fillRect(0, 0, width, height);
+  //   ctx.fillStyle = "black";
 
-    const circleSize = 1;
-    const numCirclesByRow = width / (circleSize * 10);
-    const numCirclesByColumn = height / (circleSize * 10);
-    const columnWidth = width / numCirclesByColumn;
-    const columnHeight = height / numCirclesByRow;
-    const move = { velocity: 0.01, amplitude: 100 };
-    for (let rowPosition = 0; rowPosition < numCirclesByRow; rowPosition++) {
-      for (
-        let columnPosition = 0;
-        columnPosition < numCirclesByColumn;
-        columnPosition++
-      ) {
-        const movingPositionY = Math.cos(
-          (frameCount + rowPosition * 10) * move.velocity
-        );
-        const positionY =
-          columnHeight * rowPosition +
-          columnHeight / 2 +
-          movingPositionY * move.amplitude;
+  //   const circleSize = 1;
+  //   const numCirclesByRow = width / (circleSize * 10);
+  //   const numCirclesByColumn = height / (circleSize * 10);
+  //   const columnWidth = width / numCirclesByColumn;
+  //   const columnHeight = height / numCirclesByRow;
+  //   const move = { velocity: 0.01, amplitude: 100 };
+  //   for (let rowPosition = 0; rowPosition < numCirclesByRow; rowPosition++) {
+  //     for (
+  //       let columnPosition = 0;
+  //       columnPosition < numCirclesByColumn;
+  //       columnPosition++
+  //     ) {
+  //       const movingPositionY = Math.cos(
+  //         (frameCount + rowPosition * 10) * move.velocity
+  //       );
+  //       const positionY =
+  //         columnHeight * rowPosition +
+  //         columnHeight / 2 +
+  //         movingPositionY * move.amplitude;
 
-        const movingPositionX = Math.sin(
-          (frameCount + columnPosition * 10) * move.velocity
-        );
-        const positionX =
-          columnWidth * columnPosition +
-          columnWidth / 2 +
-          movingPositionX * move.amplitude;
-        ctx.beginPath();
-        ctx.arc(positionX, positionY, circleSize, 0, 2 * Math.PI);
-        ctx.fill();
-        ctx.closePath();
-      }
-    }
+  //       const movingPositionX = Math.sin(
+  //         (frameCount + columnPosition * 10) * move.velocity
+  //       );
+  //       const positionX =
+  //         columnWidth * columnPosition +
+  //         columnWidth / 2 +
+  //         movingPositionX * move.amplitude;
+  //       ctx.beginPath();
+  //       ctx.arc(positionX, positionY, circleSize, 0, 2 * Math.PI);
+  //       ctx.fill();
+  //       ctx.closePath();
+  //     }
+  //   }
 
-    frameCount++;
+  //   frameCount++;
 
-    requestAnimationFrame(drawAnimateCircles);
-  }
+  //   requestAnimationFrame(drawAnimateCircles);
+  // }
+}
+
+function drawCircle(ctx, positionX, positionY, radius) {
+  ctx.beginPath();
+  // arc(x, y, radius, startAngle, endAngle, counterclockwise)
+  ctx.arc(positionX, positionY, radius, 0, 2 * Math.PI);
+  ctx.fill();
+  ctx.closePath();
 }
 
 function drawCircles(ctx, width, height) {
-  ctx.fillStyle = "black";
+  ctx.fillStyle = "white";
   const numCircles = 10;
   const columnWidth = width / numCircles;
-  const columnHeight = height / numCircles;
+  const lineHeight = height / numCircles;
   for (let i = 0; i < numCircles; i++) {
-    ctx.beginPath();
     const positionX = columnWidth * i + columnWidth / 2;
     for (let j = 0; j < numCircles; j++) {
-      const positionY = columnHeight * j + columnHeight / 2;
-      ctx.arc(positionX, positionY, columnWidth / 4, 0, 2 * Math.PI);
+      const positionY = lineHeight * j + lineHeight / 2;
+      drawCircle(ctx, positionX, positionY, columnWidth / 4);
     }
-    ctx.closePath();
-    ctx.fill();
   }
 }
