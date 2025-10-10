063fb069c6893d1456ba5bf757f75250b026dc0d
Gwenaëlle Gustin <gwenaelle.gustin@hevs.ch>
2025-10-10 15:30:04 +0200

    json object displayed

--- appendix_Manipulating_DOM/js/pages/projects.js
+++ appendix_Manipulating_DOM/js/pages/projects.js
@@ -2,14 +2,32 @@ import { fetchContent } from "../lib/api.js";
 
 async function main() {
   const content = await fetchContent("./../assets/data/", "content.json");
-
   const parentContainer = document.getElementById("projects-container");
 
   for (let i = 0; i < content.projects.length; i++) {
-    const element = content.projects[i];
+    const container = document.createElement("div");
+    container.classList.add("project-container");
+
+    const title = createTitle(i);
+    const image = createImage(i);
+    createImage(i);
+    container.appendChild(title);
+    container.appendChild(image);
+    parentContainer.appendChild(container);
+  }
+
+  function createTitle(i) {
     const titleElement = document.createElement("h2");
     titleElement.innerText = content.projects[i].title;
-    parentContainer.appendChild(titleElement);
+    return titleElement;
+  }
+  function createImage(i) {
+    const imageElement = document.createElement("img");
+    const path = "./../assets/images/";
+    imageElement.classList.add("thumb");
+    const src = content.projects[i].images[0] + ".jpeg";
+    imageElement.src = path + src;
+    return imageElement;
   }
 }
 
